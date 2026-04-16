5d03983ff1f6f0d13c06a9f387fb8514540f3b57
rchowd9 <135185247+rchowd9@users.noreply.github.com>
2026-04-16 15:44:34 -0400

    Implement rectangleCollision function
    Add rectangleCollision function to detect collisions between two rectangles.

--- slipfiles/game.js
+++ slipfiles/game.js
@@ -145,6 +145,16 @@ window.addEventListener('keydown', ({ key}) => {
     }
 });
 
+function rectangleCollision({rectangle1, rectangle2})
+{
+    return (
+        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
+        rectangle1.position.y <= rectangle2.position.y + rectangle2.height
+    );
+}
+
 function animate()
 {
     window.requestAnimationFrame(animate);
